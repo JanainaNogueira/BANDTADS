79e2c3bcac2767dd9bdacd876f32187a0d6be12f
ingridcriistine <ingridrochacristine@gmail.com>
2026-03-07 15:33:23 -0300

    new branch autocadastro

---
+++ FrontEnd/src/app/User/autocadastro/autocadastro.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormTitle } from '../../components/form-title/form-title';
+
+@Component({
+  selector: 'app-autocadastro',
+  imports: [FormTitle],
+  templateUrl: './autocadastro.html',
+  styleUrl: './autocadastro.css',
+})
+export class Autocadastro {
+
+}
--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Home } from './User/home/home';
+import { Autocadastro } from './User/autocadastro/autocadastro';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'home',
         component: Home
+    },
+    {
+        path: 'autocadastro',
+        component: Autocadastro
     }
 ];

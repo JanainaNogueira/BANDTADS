62aaf93568f125c33534a01679eab08cb65cd354
ingridcriistine <ingridrochacristine@gmail.com>
2026-03-14 13:48:23 -0300

    component manager-top-panel

--- FrontEnd/src/app/Gerente/home/home.ts
+++ FrontEnd/src/app/Gerente/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MenuGerente } from '../../components/menu-gerente/menu-gerente';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { ManagerTopPanel } from '../../components/manager-top-panel/manager-top-panel';
 
 @Component({
-  selector: 'app-home',
-  imports: [MenuGerente, MatIconModule, CommonModule],
+  selector: 'app-home-gerente',
+  imports: [MenuGerente, MatIconModule, CommonModule, ManagerTopPanel],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })

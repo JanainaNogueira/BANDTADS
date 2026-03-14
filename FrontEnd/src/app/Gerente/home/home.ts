2d33a7a81cb3bf45fa2e3692633f58ff1ccf8b7f
ingridcriistine <ingridrochacristine@gmail.com>
2026-03-14 12:09:04 -0300

    ajustando menu

--- FrontEnd/src/app/Gerente/home/home.ts
+++ FrontEnd/src/app/Gerente/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuGerente } from '../../components/menu-gerente/menu-gerente';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MenuGerente, MatIconModule, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })

1c9ffd83cfb88727c44c56bd6de6b4d42f7446f5
milena579 <milecalegari@gmail.com>
2026-03-28 13:50:56 -0300

    feat: corrigindo login e redirecionamento

--- FrontEnd/src/app/models/manager.model.ts
+++ FrontEnd/src/app/models/manager.model.ts
@@ -1,12 +1,23 @@
+import { ManagerStatus } from "../../assets/mock/managers.mock";
+
+export interface ManagerSummary {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  clients: number;
+  status: ManagerStatus;
+}
+
 export interface Manager {
   name: string;
   cpf: string;
 }
 
 export interface ManagerCreateEdit{
-  nome: string;
-  cpf: string;
-  telefone: string;
+  name: string;
+  cpf?: string;
+  telefone?: string;
   email: string;
   senha: string;
 }

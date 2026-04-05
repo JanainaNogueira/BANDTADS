22aad0e2c480964070a0700347ce86f03c4f985e
JanainaNogueira <janainanogueira464@gmail.com>
2026-04-05 16:23:53 -0300

    feature: vincula tela aon mock de clientes

--- FrontEnd/src/app/Gerente/customers-page/top-clientes/top-clientes.ts
+++ FrontEnd/src/app/Gerente/customers-page/top-clientes/top-clientes.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIcon } from "@angular/material/icon";
+import { MOCK_CUSTOMERS } from '../../../../assets/mock/customers.mock';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-top-clientes',
-  imports: [MatIcon],
+  standalone: true,
+  imports: [MatIcon, CommonModule],
   templateUrl: './top-clientes.html',
   styleUrl: './top-clientes.css',
 })
-export class TopClientes {
+export class TopClientes implements OnInit {
 
+  top3: any[] = [];
+
+  ngOnInit(): void {
+    this.top3 = [...MOCK_CUSTOMERS]
+      .sort((a, b) => b.balance - a.balance) 
+      .slice(0, 3);
+  }
+
+  formatarValor(valor: number): string {
+    return valor.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    });
+  }
 }

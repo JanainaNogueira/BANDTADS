e8b90e1041a65d1ea005e693be419f6f0c3b889b
milena579 <milecalegari@gmail.com>
2026-05-01 14:23:27 -0300

    removendo pasta core e arrumando ts de listagem de gerentes

--- FrontEnd/src/app/Administrador/adm-manager/services/manager.service.ts
+++ FrontEnd/src/app/Administrador/adm-manager/services/manager.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
-import { ManagerSummary, ManagerStatus } from '../models/manager.model';
+import { ManagerSummary, ManagerStatus } from '../../../models/manager.model';
 
 interface GerenteResponse {
   id: number;

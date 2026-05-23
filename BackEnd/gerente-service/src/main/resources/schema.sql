9fae49bc3b246b2492c022a79ac4f318bbfc5464
milena579 <milecalegari@gmail.com>
2026-05-23 14:05:59 -0300

    arrumando o banco

--- BackEnd/gerente-service/src/main/resources/schema.sql
+++ BackEnd/gerente-service/src/main/resources/schema.sql
@@ -1,11 +1,9 @@
-use bantads_gerente_admin;
-
-create table IF NOT EXISTS GerenteAdmin (
-	id int PRIMARY KEY auto_increment NOT NULL,
-    nome varchar(100) not null,
-	cpf varchar (11) UNIQUE not null,
+CREATE TABLE IF NOT EXISTS GerenteAdmin (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cpf VARCHAR(11) UNIQUE NOT NULL,
     telefone VARCHAR(11) NOT NULL,
-    email varchar(150) not null,
-    senha varchar(15) not null
-    tipo varchar(10) not null
+    email VARCHAR(150) NOT NULL,
+    senha VARCHAR(15) NOT NULL,
+    tipo VARCHAR(10) NOT NULL
 );

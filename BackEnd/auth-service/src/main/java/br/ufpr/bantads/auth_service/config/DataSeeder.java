b7b4b1f68b628f5e54d6effe155aa52caea29673
mariaconradtech <mariajuliaandradeconrad@gmail.com>
2026-05-31 22:32:21 -0300

    fix: corrige scripts SQL e DataSeeder com dados pré-cadastrados completos

--- BackEnd/auth-service/src/main/java/br/ufpr/bantads/auth_service/config/DataSeeder.java
+++ BackEnd/auth-service/src/main/java/br/ufpr/bantads/auth_service/config/DataSeeder.java
@@ -20,25 +20,82 @@ public class DataSeeder implements CommandLineRunner {
             // Hash SHA-256 para a senha "tads" concatenada com o salt "tads" ("tadstads")
             String senhaHash = "b484db7b96fe051c150cefd61a5befa786bcba5a113a3bef33152a81c317a89b";
 
-            // Cadastra o Administrador
+            // Administrador
             usuarioRepository.save(new Usuario(
-                    null, 
-                    "adm1@bantads.com.br", 
-                    senhaHash, 
-                    "ADMINISTRADOR", 
-                    "40501740066" // CPF que o teste espera encontrar
+                    null,
+                    "adm1@bantads.com.br",
+                    senhaHash,
+                    "ADMINISTRADOR",
+                    "40501740066"
             ));
 
-            // Cadastro do Gerente de teste padrão
+            // Gerentes
             usuarioRepository.save(new Usuario(
-                    null, 
-                    "ger3@bantads.com.br", 
-                    senhaHash, 
-                    "GERENTE", 
-                    "00000000000" 
+                    null,
+                    "ger1@bantads.com.br",
+                    senhaHash,
+                    "GERENTE",
+                    "98574307084"   // Geniéve
             ));
 
-            System.out.println(">>> [BANTADS] MongoDB populado com os usuários de teste com sucesso! <<<");
+            usuarioRepository.save(new Usuario(
+                    null,
+                    "ger2@bantads.com.br",
+                    senhaHash,
+                    "GERENTE",
+                    "64065268052"   // Godophredo
+            ));
+
+            usuarioRepository.save(new Usuario(
+                    null,
+                    "ger3@bantads.com.br",
+                    senhaHash,
+                    "GERENTE",
+                    "23862179060"   // Gyândula
+            ));
+
+            // Clientes
+            usuarioRepository.save(new Usuario(
+                    null,
+                    "cli1@bantads.com.br",
+                    senhaHash,
+                    "CLIENTE",
+                    "12912861012"   // Catharyna
+            ));
+
+            usuarioRepository.save(new Usuario(
+                    null,
+                    "cli2@bantads.com.br",
+                    senhaHash,
+                    "CLIENTE",
+                    "09506382000"   // Cleuddônio
+            ));
+
+            usuarioRepository.save(new Usuario(
+                    null,
+                    "cli3@bantads.com.br",
+                    senhaHash,
+                    "CLIENTE",
+                    "85733854057"   // Catianna
+            ));
+
+            usuarioRepository.save(new Usuario(
+                    null,
+                    "cli4@bantads.com.br",
+                    senhaHash,
+                    "CLIENTE",
+                    "58872160006"   // Cutardo
+            ));
+
+            usuarioRepository.save(new Usuario(
+                    null,
+                    "cli5@bantads.com.br",
+                    senhaHash,
+                    "CLIENTE",
+                    "76179646090"   // Coândrya
+            ));
+
+            System.out.println(">>> [BANTADS] MongoDB populado com 9 usuários (1 admin + 3 gerentes + 5 clientes) <<<");
         }
     }
 }

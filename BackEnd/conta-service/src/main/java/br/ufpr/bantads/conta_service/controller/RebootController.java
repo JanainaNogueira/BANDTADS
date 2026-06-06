f98811103c52b489d735759607194680727d45c6
milena579 <milecalegari@gmail.com>
2026-06-05 21:23:23 -0300

    fix: adicionando id de gerente a criaçao de conta reboot

--- BackEnd/conta-service/src/main/java/br/ufpr/bantads/conta_service/controller/RebootController.java
+++ BackEnd/conta-service/src/main/java/br/ufpr/bantads/conta_service/controller/RebootController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import br.ufpr.bantads.conta_service.model.Conta;
 import br.ufpr.bantads.conta_service.repository.write.ContaWriteRepository;
@@ -21,34 +22,40 @@ public class RebootController {
     }
 
     @GetMapping("/reboot")
-    public ResponseEntity<Void> reboot() {
+public ResponseEntity<Void> reboot() {
+    repo.deleteAll();
 
-        repo.deleteAll();
+    Integer idGenieve    = buscarGerenteId("98574307084");
+    Integer idGodophredo = buscarGerenteId("64065268052");
+    Integer idGyandula   = buscarGerenteId("23862179060");
 
-        Conta c1 = criarConta(1, "1291", "12912861012");
-        Conta c2 = criarConta(2, "0950", "09506382000");
-        Conta c3 = criarConta(3, "8573", "85733854057");
-        Conta c4 = criarConta(4, "5887", "58872160006");
-        Conta c5 = criarConta(5, "7617", "76179646090");
+    Conta c1 = criarConta(1, "1291", idGenieve);
+    Conta c2 = criarConta(2, "0950", idGodophredo);
+    Conta c3 = criarConta(3, "8573", idGyandula);
+    Conta c4 = criarConta(4, "5887", idGenieve);
+    Conta c5 = criarConta(5, "7617", idGodophredo);
 
-        repo.saveAll(List.of(c1, c2, c3, c4, c5));
+    repo.saveAll(List.of(c1, c2, c3, c4, c5));
+    return ResponseEntity.ok().build();
+}
 
-        return ResponseEntity.ok().build();
-    }
+private Integer buscarGerenteId(String cpf) {
+    RestTemplate rest = new RestTemplate();
+    var resp = rest.getForObject(
+        "http://gerente-service:8080/gerentes/" + cpf,
+        java.util.Map.class
+    );
+    return (Integer) resp.get("id");
+}
 
-    private Conta criarConta(Integer clienteId, String numeroConta, String cpfCliente) {
-
-        Conta c = new Conta();
-
-        c.setClienteId(clienteId);
-        c.setNumeroConta(numeroConta);
-        c.setDataCriacao(LocalDateTime.now());
-
-        c.setSaldo(BigDecimal.ZERO);
-        c.setLimite(BigDecimal.ZERO); // importante: teste começa e calcula depois
-
-        c.setGerenteId(null); // ou setado depois pelo sistema
-
-        return c;
-    }
+private Conta criarConta(Integer clienteId, String numeroConta, Integer gerenteId) {
+    Conta c = new Conta();
+    c.setClienteId(clienteId);
+    c.setNumeroConta(numeroConta);
+    c.setDataCriacao(LocalDateTime.now());
+    c.setSaldo(BigDecimal.ZERO);
+    c.setLimite(BigDecimal.ZERO);
+    c.setGerenteId(gerenteId);
+    return c;
+}
 }
